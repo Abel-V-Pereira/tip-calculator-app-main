26ca71f4fa81bfe4db9e9e534425edaa9fd55aff
Abel Viegas Pereira <abelviegaspereira@gmail.com>
2025-10-15 20:12:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+I think I got better at working with forEach and Event Listeners, it's still far from good but it's definitely better than in my past codes XD.
+
+### Continued development
+
+I want to get better at refactoring my code. Sometimes I just can't figure out how to transform my repetitive code into some function that'll do things in a cleaner way. I gotta work harder on that for sure.
+
+## Author
+
+- Frontend Mentor - [@Abel-V-Pereira](https://www.frontendmentor.io/profile/Abel-V-Pereira)
---
+++ index.js
@@ -0,0 +1,124 @@
+const billInput = document.getElementById('bill');
+const customInput = document.getElementById('custom');
+const peopleInput = document.getElementById('people');
+const resetButton = document.querySelector('.reset-button');
+const tipButtons = document.querySelectorAll('[data-tip]');
+const tipAmount = document.getElementById('tip-amount');
+const totalAmount = document.getElementById('total-amount');
+const errorMessageBill = document.querySelector('.error-message-bill');
+const errorMessagePeople = document.querySelector('.error-message-people');
+let bill = 0;
+let tip = 0;
+let people = 0;
+
+billInput.addEventListener('input', (e) => {
+    bill = e.target.value;
+    billSpliter(bill, tip, people);
+
+    if (bill === '') {
+        billInput.classList.remove('error');
+        billInput.classList.remove('active');
+        errorMessageBill.style.display = 'none';
+    } else if (bill > 0) {
+        billInput.classList.remove('error');
+        billInput.classList.add('active');
+        billInput.style.outline = 'none';
+        errorMessageBill.style.display = 'none';
+    } else {
+        billInput.classList.add('error');
+        billInput.classList.remove('active');
+        billInput.style.outline = 'none';
+        errorMessageBill.style.display = 'block';
+    }
+});
+
+customInput.addEventListener('input', (e) => {
+    tip = e.target.value;
+    billSpliter(bill, tip, people);
+
+    tipButtons.forEach(btn => btn.classList.remove('selected'));
+
+    if (tip === '') {
+        customInput.classList.remove('error');
+        customInput.classList.remove('active');
+    } else if (tip > 0) {
+        customInput.classList.remove('error');
+        customInput.classList.add('active');
+        customInput.style.outline = 'none';
+    } else {
+        customInput.classList.add('error');
+        customInput.classList.remove('active');
+        customInput.style.outline = 'none';
+    }
+});
+
+peopleInput.addEventListener('input', (e) => {
+    people = e.target.value;
+    billSpliter(bill, tip, people);
+
+    if (people === '') {
+        peopleInput.classList.remove('error');
+        peopleInput.classList.remove('active');
+        errorMessagePeople.style.display = 'none'
+    } else if (people > 0) {
+        peopleInput.classList.remove('error');
+        peopleInput.classList.add('active');
+        peopleInput.style.outline = 'none';
+        errorMessagePeople.style.display = 'none';
+    } else {
+        peopleInput.classList.add('error');
+        peopleInput.classList.remove('active');
+        peopleInput.style.outline = 'none';
+        errorMessagePeople.style.display = 'block';
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    billInput.value = '';
+    bill = 0;
+    customInput.value = '';
+    tip = 0;
+    peopleInput.value = '';
+    people = 0;
+    totalAmount.innerText = "$0.00"
+    tipAmount.innerText = "$0.00"
+
+    billInput.classList.remove('error');
+    billInput.classList.remove('active');
+    errorMessageBill.style.display = 'none';
+    billInput.style.outline = 'none';
+    customInput.classList.remove('error');
+    customInput.classList.remove('active');
+    customInput.style.outline = 'none';
+    peopleInput.classList.remove('error');
+    peopleInput.classList.remove('active');
+    errorMessagePeople.style.display = 'none';
+    peopleInput.style.outline = 'none';
+    
+    tipButtons.forEach(btn => btn.classList.remove('selected'));
+
+});
+
+tipButtons.forEach(button => button.addEventListener('click', () => {
+    tip = button.dataset.tip;
+    billSpliter(bill, tip, people);
+    customInput.value = '';
+    customInput.classList.remove('error');
+    customInput.classList.remove('active');
+    customInput.style.outline = 'none';
+
+    tipButtons.forEach(btn => btn.classList.remove('selected'));
+    button.classList.add('selected');
+
+}));
+
+function billSpliter(bill, tip, people) {
+    if (bill < 1 || tip < 1 || people < 1) {
+        totalAmount.innerText = "$0.00"
+        tipAmount.innerText = "$0.00"
+    } else {
+        totalAmount.innerText = `$${(bill / people).toFixed(2)}`
+        tipAmount.innerText = `$${((tip * bill / 100) / people).toFixed(2)}`
+
+    }
+}
